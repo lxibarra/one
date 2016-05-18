8d80cae163e0d25205ffd78831c40429d50c97dc
Ricardo Ibarra <coder10@gmail.com>
2016-05-17 23:15:03 -0700

    Started login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Login extend React.Component {
+  render() {
+      return (
+        <div>
+          <h1>Login page</h1>
+          <p>Implement login stuff here</p>
+          <p>Using Facebook, Twitter, Github & Google</p>
+          <p>
+            Implement react router to navigate from login to the game table.
+            Also use firebase as a stateless game tracker.
+          </p>
+        </div>
+      )
+  }
+}
+
+export default Login;
