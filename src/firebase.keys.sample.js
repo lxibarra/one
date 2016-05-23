815b0225f65a80d93abff3115debc0869711fb88
Ricardo Ibarra <coder10@gmail.com>
2016-05-22 21:32:53 -0700

    firebase login integration in process

---
+++ src/firebase.keys.sample.js
@@ -0,0 +1,10 @@
+//At a later point this will be environment variables
+//
+//The following information is provided by firebase in the app console page
+export default
+  {
+    apiKey: "your-api-key",
+    authDomain: "your-firebase-auth-domain",
+    databaseURL: "your-database-url",
+    storageBucket: "your-store-bucket",
+  }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,44 @@
+/*
+  Firebase auth docs
+  https://firebase.google.com/docs/auth/#key_functions
+  Documentation for twitter authentication
+  https://firebase.google.com/docs/auth/web/twitter-login#redirect-mode
+
+*/
+
 import React from 'react';
 import {Link} from 'react-router';
+import Firebase from 'firebase';
+import { browserHistory } from 'react-router';
+import firebaseConfig from '../firebase.keys.js';
+
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentWillMount() {
+    this.setState({ loginError:'hide'});
+
+     Firebase.initializeApp(firebaseConfig);
+  }
+
+  twitterLogin() {
+    this.setState({
+      loginError:'hide'
+    });
+    let provider = new Firebase.auth.TwitterAuthProvider();
+    Firebase.auth().signInWithPopup(provider).then((result)=>{
+      browserHistory.push('/play');
+    }).catch((error)=>{
+      this.setState({
+        loginError:'show'
+      });
+    })
+  }
+
   render() {
 
       let containerStyle = {
@@ -12,6 +49,7 @@ class Login extends React.Component {
           'margin':'0 auto',
           'maxWidth':500
       };
+
 
       return (
         <div style={containerStyle}>
@@ -24,11 +62,20 @@ class Login extends React.Component {
                           <p>Please login using one of the following methods</p>
                           <div className="text-center">
                              <div className="">
-                                <Link to="/play" className="btn btn-info btn-block">Twitter <i className="fa fa-twitter"></i></Link>
+                                <button onClick={this.twitterLogin.bind(this)} className="btn btn-info btn-block">Twitter <i className="fa fa-twitter"></i></button>
                                 <Link to="/play" className="btn btn-primary btn-block">Facebook <i className="fa fa-facebook"></i></Link>
                                 <Link to="/play" className="btn btn-default btn-block">Github <i className="fa fa-github-alt"></i> </Link>
                                 <Link to="/play" className="btn btn-danger btn-block">Google <i className="fa fa-google-plus"></i></Link>
                              </div>
+                          </div>
+
+                          <div className={this.state.loginError}>
+                              <hr/>
+                            <div className="alert alert-danger" role="alert">
+                              <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                              <span className="sr-only">Error:</span>
+                              Authentication Error, please try again.
+                            </div>
                           </div>
                     </div>
                   </div>
